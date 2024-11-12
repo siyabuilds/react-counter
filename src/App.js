446c18f81d58eec34760d9ad53b109c32b33cccd
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2024-11-12 13:49:45 +0200

    Use header in main App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Counter from "./Counter";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Counter></Counter>
     </div>
   );
